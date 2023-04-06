94712d050ac41011c3d8cef7af728795732be884
Joshua W <106320744+J-Wil21@users.noreply.github.com>
2023-04-06 13:11:49 +0100

    Added interactivity when type info.

--- app.js
+++ app.js
@@ -7,15 +7,63 @@ const cvc = document.querySelector('#cvc');
 // output data
 const showNum = document.querySelector('#showNum');
 const showName = document.querySelector('#showName');
-const showExpDate = document.querySelector('#showExpDate');
+const showExpDateM = document.querySelector('#showExpDateM');
+const showExpDateY = document.querySelector('#showExpDateY');
 const showCvc = document.querySelector('#showCvc');
 
 const btn = document.querySelector('#submit')
 
+// message
+const message = document.querySelector('.hide');
+const box = document.getElementById('box')
+
+
+btn.addEventListener('click', () => {
+
+    box.classList.toggle('message');
+
+
+})
+
+
+
 cardName.addEventListener('input', () => {
-    if (cardName.value == " ") {
+    if (cardName.value === "") {
         showName.innerText = "Full name";
     } else {
         showName.innerHTML = cardName.value;
     }
-})
+});
+
+cardNum.addEventListener('input', () => {
+    if (cardNum.value === "") {
+        showNum.innerText = "0000 0000 0000 0000";
+    } else {
+        showNum.innerHTML = cardNum.value;
+    }
+});
+
+expDateM.addEventListener('input', () => {
+    if (expDateM.value === "") {
+        showExpDateM.innerText = "00";
+    } else {
+        showExpDateM.innerHTML = expDateM.value;
+    }
+});
+
+expDateY.addEventListener('input', () => {
+    if (expDateY.value === "") {
+        showExpDateY.innerText = "00";
+    } else {
+        showExpDateY.innerHTML = expDateY.value;
+    }
+});
+
+cvc.addEventListener('input', () => {
+    if (cvc.value === "") {
+        showCvc.innerText = "000";
+    } else {
+        showCvc.innerHTML = cvc.value;
+    }
+});
+
