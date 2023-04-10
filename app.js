7e43751d9ec506fcaa0c2ab1e3fbc0a8bd34467d
Joshua W <106320744+J-Wil21@users.noreply.github.com>
2023-04-10 07:51:16 +0100

    worked on interactivity

--- app.js
+++ app.js
@@ -7,15 +7,72 @@ const cvc = document.querySelector('#cvc');
 // output data
 const showNum = document.querySelector('#showNum');
 const showName = document.querySelector('#showName');
-const showExpDate = document.querySelector('#showExpDate');
+const showExpDateM = document.querySelector('#showExpDateM');
+const showExpDateY = document.querySelector('#showExpDateY');
 const showCvc = document.querySelector('#showCvc');
 
-const btn = document.querySelector('#submit')
+const btn = document.querySelector('#btn')
+
+// message
+const message = document.querySelector('.hide');
+const box = document.getElementById('box')
+const closeBtn = document.querySelector('.close-btn')
+
+
+btn.addEventListener('click', () => {
+    if (message.style.display === 'none') {
+        message.style.display = 'block'
+    } else {
+        message.style.display = 'none'
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    if (message.style.display === 'none') {
+        message.style.display = 'block'
+    } else {
+        message.style.display = 'none'
+    }
+})
+
 
 cardName.addEventListener('input', () => {
-    if (cardName.value == " ") {
+    if (cardName.value === "") {
         showName.innerText = "Full name";
     } else {
         showName.innerHTML = cardName.value;
     }
-})
+});
+
+cardNum.addEventListener('input', () => {
+    if (cardNum.value === "") {
+        showNum.innerText = "0000 0000 0000 0000";
+    } else {
+        showNum.innerHTML = cardNum.value;
+    }
+});
+
+expDateM.addEventListener('input', () => {
+    if (expDateM.value === "") {
+        showExpDateM.innerText = "00";
+    } else {
+        showExpDateM.innerHTML = expDateM.value;
+    }
+});
+
+expDateY.addEventListener('input', () => {
+    if (expDateY.value === "") {
+        showExpDateY.innerText = "00";
+    } else {
+        showExpDateY.innerHTML = expDateY.value;
+    }
+});
+
+cvc.addEventListener('input', () => {
+    if (cvc.value === "") {
+        showCvc.innerText = "000";
+    } else {
+        showCvc.innerHTML = cvc.value;
+    }
+});
+
